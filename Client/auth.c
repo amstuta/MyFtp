9ab198e5540dab28d6367394fe9e428718bcc993
amstut_a <arthur.amstutz@epitech.eu>
2015-03-24 13:39:19 +0100

    rendu by amstut_a on Manjaro @ mar. mars 24 13:39:19 CET 2015

--- Client/auth.c
+++ Client/auth.c
@@ -15,28 +15,28 @@
 
 int			check_user(int fd)
 {
-  int			rd;
   char			tmp[LINE_SIZE];
   char			login[LINE_SIZE];
 
   memset(tmp, 0, LINE_SIZE);
-  write(1, "Please enter your login: ", 25);
-  if ((rd = read(0, tmp, LINE_SIZE)) <= 0)
-    return (-1);
-  tmp[rd] = 0;
   memset(login, 0, LINE_SIZE);
-  strcat(login, "USER ");
-  strcat(login, tmp);
-  strcat(login, "\r\n");
+  strcat(login, "USER Anonymous\r\n");
   write(fd, login, strlen(login));
   memset(tmp, 0, LINE_SIZE);
   if (read(fd, tmp, LINE_SIZE) == -1)
     return (-1);
-  if (strcmp(tmp, "331"))
+  if (!strncmp(tmp, "230", 3))
+    return (0);
+  else if (!strncmp(tmp, "331", 3))
     {
-      write(1, "Wrong user, exiting...\n", 23);
-      return (-1);
+      write(fd, "PASS \r\n", 7);
+      if (read(fd, tmp, LINE_SIZE) == -1)
+	return (-1);
+      if (strcmp(tmp, "230"))
+	return (-1);
     }
+  else
+    return (-1);
   return (0);
 }
 
@@ -50,11 +50,6 @@ int			auth_to_server(int fd)
   if (strcmp(tmp, "220"))
     return (-1);
   if (check_user(fd) == -1)
-    return (-1);
-  write(fd, "PASS \n\r", 7);
-  if (read(fd, tmp, LINE_SIZE) == -1)
-    return (-1);
-  if (strcmp(tmp, "230"))
     return (-1);
   return (0);
 }
